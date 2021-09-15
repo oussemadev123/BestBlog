bf40d9413692ed14d980686394e5391ccc1b681f
oussemadev123 <oussemachaarifcb789@gmail.com>
2021-09-15 13:43:11 +0100

    create database

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>{{ env('APP_NAME','BestBlog') }}</title>
-    
 </head>
 <body>
     @include('includes.navbar')
